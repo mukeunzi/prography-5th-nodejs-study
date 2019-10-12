9ea9c680d0d7363b0c3c0f3295099b6473bb020f
mukeunzi <eunji057@gmail.com>
2019-10-12 10:25:20 +0900

    refactor : json 파일명 및 파일 기본 값 변경

--- src/database/connector.js
+++ src/database/connector.js
@@ -1,9 +1,9 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync('../db/example.json');
+const adapter = new FileSync('../db/data.json');
 const db = low(adapter);
 
-db.defaults({ todos: [] }).write();
+db.defaults({ users: [], books: [] }).write();
 
 module.exports = db;
